8971ebd4c65899baef8600188fafc1f6cbe67865
Nico <nico@beispiel.de>
2026-03-26 20:20:22 +0100

    Design der Startseite angepasst und Texte aktualisiert

---
+++ script.js
@@ -0,0 +1,13 @@
+function showContent(category) {
+    const content = document.querySelectorAll(".content_div");
+
+    content.forEach(content => {
+        content.classList.remove("show");
+    })
+
+    const activeContent = document.getElementById("content_" + category);
+
+    if (activeContent) {
+        activeContent.classList.add("show");
+    }
+}
